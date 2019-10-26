b9276e99e20e9afbbb61224f6196c415672787b4
nfilskov <niels.f@htomail.fr>
2019-10-26 20:53:28 +0200

    site pret sans lien de téléchargement et sans support

--- js/app.js
+++ js/app.js
@@ -4,6 +4,35 @@ $(function() {
     });
 });
 
+Email.send({
+    Host : "ex2.mail.ovh.net",
+    Username : "nfilskov@mxm.eu",
+    Password : "password",
+    To : 'nfilskov@mxm.eu',
+    From : "niels.f@hotmail.fr",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+
+function sendMail() {
+
+	var subject = document.getElementById("subject").value;
+	var pseudo = document.getElementById("pseudo").value;
+	var mail = document.getElementById("mail").value;
+	var choice = document.getElementById("choice");
+	var optionChoice = choice.options[choice.selectedIndex].text;
+
+    var link = "mailto:niels.f@hotmail.fr"
+             + "?cc=nfilskov@mxm.eu"
+             + "&subject=" + escape(subject)
+             + "&body=" + escape(optionChoice) + " " escape(document.getElementById('message').value)
+    ;
+
+    window.location.href = link;
+}
+
 
 // TRANSLATION PART
 // $.getJSON( "core/translation.json", function( data ) {
@@ -17,3 +46,4 @@ $(function() {
 //       html: items.join( "" )
 //     }).appendTo( "body" );
 // });
+
